35cda416be5c87f18c9300a1e687e6efa5ef8032
Antonia Elek <antoniaelek@hotmail.com>
2018-10-13 02:18:24 +0200

    added select widgets to aggregate visual

--- bokeh/aggregate.py
+++ bokeh/aggregate.py
@@ -1,7 +1,7 @@
 from bokeh.models import CategoricalColorMapper
 from bokeh.io import curdoc
 from bokeh.plotting import figure
-from bokeh.layouts import gridplot, column
+from bokeh.layouts import gridplot, column, layout
 from bokeh.models import ColumnDataSource
 from bokeh.models.widgets import Select
 from bokeh.palettes import d3
@@ -16,37 +16,110 @@ SEASON = "2018-19"
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))   # refers to application_top
 BASE_PATH = APP_ROOT + "/"
 
-df = functions.get_detailed_data(BASE_PATH, SEASON)
+df = functions.get_detailed_aggregate_data(BASE_PATH, SEASON)
+
+
+x_metric = "assists"
+y_metric = "assists"
+x_agg_func = "mean"
+y_agg_func = "mean"
 
 
 def normalize_circle_size(row):
     return 0.1
 
 
-# set data source for visual
-source = ColumnDataSource(data=dict(
-    name=df['name'],
-    mean_ict_index=df['mean_ict_index'],
-    mean_value=df['mean_value'],
-    circle_size=df.apply(normalize_circle_size, axis=1)))
+def update_x_agg_func(attrname, old, new):
+    global x_agg_func, p
+    x_agg_func = new
+    source.data=dict(
+        name=df['name'],
+        x=df[x_agg_func + '_' + x_metric],
+        y=df[y_agg_func + '_' + y_metric],
+        x_title=[x_agg_func + "_" + x_metric]*len(df['name']),
+        y_title=[y_agg_func + "_" + y_metric]*len(df['name']),
+        circle_size=[p.x_range.default_span/200]*len(df['name']),
+        fill_alpha=[0.3]*len(df['name']))
+
+
+def update_y_agg_func(attrname, old, new):
+    global y_agg_func, p
+    y_agg_func = new
+    source.data=dict(
+        name=df['name'],
+        x=df[x_agg_func + '_' + x_metric],
+        y=df[y_agg_func + '_' + y_metric],
+        x_title=[x_agg_func + "_" + x_metric]*len(df['name']),
+        y_title=[y_agg_func + "_" + y_metric]*len(df['name']),
+        circle_size=[p.x_range.default_span/200]*len(df['name']),
+        fill_alpha=[0.3]*len(df['name']))
+
+
+def update_x_metric(attrname, old, new):
+    global x_metric, p
+    x_metric = new
+    source.data=dict(
+        name=df['name'],
+        x=df[x_agg_func + '_' + x_metric],
+        y=df[y_agg_func + '_' + y_metric],
+        x_title=[x_agg_func + "_" + x_metric]*len(df['name']),
+        y_title=[y_agg_func + "_" + y_metric]*len(df['name']),
+        circle_size=[p.x_range.default_span/200]*len(df['name']),
+        fill_alpha=[0.3]*len(df['name']))
+
+
+def update_y_metric(attrname, old, new):
+    global y_metric, p
+    y_metric = new
+    source.data=dict(
+        name=df['name'],
+        x=df[x_agg_func + '_' + x_metric],
+        y=df[y_agg_func + '_' + new],
+        x_title=[x_agg_func + "_" + x_metric]*len(df['name']),
+        y_title=[y_agg_func + "_" + new]*len(df['name']),
+        circle_size=[p.x_range.default_span/200]*len(df['name']),
+        fill_alpha=[0.3]*len(df['name']))
+
+
+select_agg_func_x = Select(options=functions.get_aggregate_functions())
+select_agg_func_y = Select(options=functions.get_aggregate_functions())
+
+select_metric_x = Select(options=functions.get_features_for_aggregation())
+select_metric_y = Select(options=functions.get_features_for_aggregation())
+
+select_agg_func_x.on_change('value', update_x_agg_func)
+select_agg_func_y.on_change('value', update_y_agg_func)
+select_metric_x.on_change('value', update_x_metric)
+select_metric_y.on_change('value', update_y_metric)
 
 tools = "hover,crosshair,pan,wheel_zoom,zoom_in,zoom_out,box_zoom,undo,redo,reset,tap,save,box_select,poly_select,lasso_select,"
 
-p = figure(tools=tools, x_axis_label="mean_ict_index", y_axis_label="mean_value")
+p = figure(tools=tools)
 p.hover.tooltips = """<table>
 <tr style="line-height: 0.8; font-size: 17px; font-weight: bold; padding:0; margin: 0">
     <td colspan=2">@name</td>
 </tr>
 <tr style="line-height: 0.8; font-size: 12px; padding:0; margin: 0">
-    <td style="font-weight: bold;">mean_ict_index</td>
-    <td>@mean_ict_index</td>
+    <td style="font-weight: bold;">@x_title</td>
+    <td>@x</td>
 </tr>
 <tr style="line-height: 0.8; font-size: 12px; padding:0; margin: 0">
-    <td style="font-weight: bold;">mean_value</td>
-    <td>@mean_value &pound;</td>
+    <td style="font-weight: bold;">@y_title</td>
+    <td>@y</td>
 </tr>
 </table>
 """
-p.scatter(x='mean_ict_index', y='mean_value', radius='circle_size', source=source)
 
-curdoc().add_root(column(p, width=600, height=600, sizing_mode="scale_width"))
+# set data source for visual
+source = ColumnDataSource(data=dict(
+    name=df['name'],
+    x=df[x_agg_func + '_' + x_metric],
+    y=df[y_agg_func + '_' + y_metric],
+    x_title=[x_agg_func + "_" + x_metric]*len(df['name']),
+    y_title=[y_agg_func + "_" + y_metric]*len(df['name']),
+    circle_size=[p.x_range.default_span/200]*len(df['name'])))
+p.scatter(x='x', y='y', radius='circle_size', source=source)
+
+fig = ()
+
+curdoc().add_root(layout([[select_agg_func_x, select_metric_x], [select_agg_func_y, select_metric_y],[p]]))
